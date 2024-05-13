b2492ca5cfb295501280194a56047c35a7ea4dc6
Abhrajyoti00 <abhrajyoti00@gmail.com>
2024-05-13 10:28:52 +0530

    Added the original unoptimised C files

---
+++ unoptimised_IMDB.h
@@ -0,0 +1,5 @@
+#pragma once 
+#include "./include/k2c_tensor_include.h" 
+void IMDB(k2c_tensor* dense_1_input_input, k2c_tensor* dense_3_output); 
+void IMDB_initialize(); 
+void IMDB_terminate(); 
